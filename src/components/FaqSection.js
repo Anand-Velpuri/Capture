93259451ec75e52625cee4c6613ada3190496ec4
Anand-Velpuri <velpurianand8005@gmail.com>
2025-01-20 10:26:24 +0530

    17.Added a Toggle Component

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import { About } from '../styles';
+import Toggle from './Toggle';
 
 const FaqSection = () => {
     return (
         <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I Start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quia.</p>
-                </div>
-            </div>
+            <Toggle>
 
-            <div className="faq-line"></div>
+                <div className="question">
+                    <h4>How Do I Start?</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quia.</p>
+                    </div>
+                </div>
+
+                <div className="faq-line"></div>
+            </Toggle>
 
             <div className="question">
                 <h4>Daily Schedule</h4>
