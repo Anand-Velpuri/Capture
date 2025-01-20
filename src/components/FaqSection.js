1f6a085e9b8562497e85bddcc8cc44dad3fbb3f9
Anand-Velpuri <velpurianand8005@gmail.com>
2025-01-20 11:00:47 +0530

    18.Finished Faq Animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,51 +2,57 @@ import React from 'react';
 import styled from 'styled-components';
 import { About } from '../styles';
 import Toggle from './Toggle';
+import { LayoutGroup } from "framer-motion"
 
 const FaqSection = () => {
     return (
         <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
-            <Toggle>
+            <LayoutGroup>
 
-                <div className="question">
-                    <h4>How Do I Start?</h4>
-                    <div className="answer">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quia.</p>
-                    </div>
-                </div>
+                <Toggle title="How Do I Start?">
 
-                <div className="faq-line"></div>
-            </Toggle>
+                
+            
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quia.</p>
+                        </div>
+                    
 
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quia.</p>
-                </div>
-            </div>
+                    
+                </Toggle>
 
-            <div className="faq-line"></div>
+                <Toggle title="Daily Schedule">
+                    
+                        
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quia.</p>
+                        </div>
+                    
+                </Toggle>
 
-            <div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quia.</p>
-                </div>
-            </div>
 
-            <div className="faq-line"></div>
+                <Toggle title="Different Payment Methods">
+                
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quia.</p>
+                        </div>
+                    
+                </Toggle>
 
-            <div className="question">
-                <h4>What Products Do You Offer.</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quia.</p>
-                </div>
-            </div>
+                <Toggle title="What Products Do You Offer.">
+                    
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quia.</p>
+                        </div>
+                    
+                </Toggle>
+
+            </LayoutGroup>
         </Faq>
     )
 }
